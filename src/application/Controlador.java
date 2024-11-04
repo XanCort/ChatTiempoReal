d61bbb435be2ae2e208ab374112458ff1a89cc8d
pxbloserr <pabloserrperez@gmail.com>
2024-11-04 18:57:57 +0100

    probar añadir funciones textArea

--- src/application/Controlador.java
+++ src/application/Controlador.java
@@ -1,6 +1,7 @@
 package application;
 
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 
 public class Controlador {
@@ -17,6 +18,21 @@ public class Controlador {
 		
 		vista1.start(stage);
 		vista2.start(stage2);
+		
+		vista1.getCampoMensaje().setOnKeyPressed(e -> {
+			if(e.isShiftDown() && e.getCode().equals(KeyCode.ENTER)) {
+				vista1.getCampoMensaje().getText();
+
+			} else if (e.getCode().equals(KeyCode.ENTER)) {
+				crearEtiqueta(vista1.getCampoMensaje().getText());
+			}
+		});
+		
+		vista2.getCampoMensaje().setOnKeyPressed(e -> {
+			if(e.getCode().equals(KeyCode.ENTER)) {
+				crearEtiqueta(vista2.getCampoMensaje().getText());
+			}
+		});
 		
 		vista1.getBtnEnviar().setOnAction(e -> {
 			crearEtiqueta(vista1.getCampoMensaje().getText());
